d1cc443999c6dceb905c3d1d3d1fb717507f8c89
lzugaj <luka.zugaj@sigit.hr>
2025-05-25 19:44:31 +0200

    Initial commit

---
+++ src/test/java/com/luv2code/weatheralertsystem/WeatherAlertSystemKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.weatheralertsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAlertSystemKafkaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
